1b1e6b0ea1512e4626a9abd210b50c22c2f178c1
tabs <jtabas@gmail.com>
2017-06-28 10:42:39 -0400

    neighborhood migration

---
+++ db/migrate/20170627203606_create_neighborhoods.rb
@@ -0,0 +1,9 @@
+class CreateNeighborhoods < ActiveRecord::Migration[5.1]
+  def change
+    create_table :neighborhoods do |t|
+      t.string :name, null: false
+      t.string :zipcode, array: true, default: [], null: false
+      t.timestamps
+    end
+  end
+end
