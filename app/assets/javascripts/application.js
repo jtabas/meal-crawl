688921d3a11671f0ac6d3f7a5306f46385313a83
tabs <jtabas@gmail.com>
2017-07-07 15:53:20 -0400

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
 //= require rails-ujs
 //= require_tree
 
-// $(function(){ $(document).foundation(); });
-
-// $(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation(); });
