4927b60567eb54639a8904b6c916a335b2211f88
tabs <jtabas@gmail.com>
2017-07-09 16:53:50 -0400

    api call changes

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -11,12 +11,13 @@ class Api::V1::RestaurantsController < ApplicationController
   private
 
   def make_api_call
-    data = JSON.parse(RestClient.get "http://freegeoip.net/json/#{request.remote_ip}")
+    data = JSON.parse(RestClient.get "http://freegeoip.net/json/2601:41:4102:4e6e:41c8:5723:2525:383d")
     lat = data["latitude"]
     long = data["longitude"]
     key = ENV['GOOGLE_API_KEY']
 
     @location = Location.find_or_create_by(lat: lat, long: long)
+
 
     response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{long}&radius=1800&type=restaurant&keyword=taco&key=#{key}",
     {:content_type => :json, :'Authorization' => ENV['GOOGLE_API_KEY'] }
@@ -25,26 +26,27 @@ class Api::V1::RestaurantsController < ApplicationController
       places = RestClient.get "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{result['place_id']}&key=#{key}"
       places = JSON.parse(places)
       restaurant = places['result']
-      zipcode = result['vicinity']
+      zipcode = 19145
       hours = nil
-      if result['opening_hours']
-        hours = result['opening_hours']['weekday_text'].join('\n')
-      end
       photo = nil
-      if restaurant['photos']
-        photo = "https://maps.googleapis.com/maps/api/place/photo?width=300,height=300&photoreference=#{restaurant['photos'].first['photo_reference']}&key=#{ENV['GOOGLE_API_KEY']}"
+      if restaurant['opening_hours']
+        hours = restaurant['opening_hours']['weekday_text'].join('\n')
       end
-
-      @restuarant = Restaurant.find_or_create_by(
-        name: result['name'],
-        address: result['vicinity'],
+      if restaurant['photos']
+        photo = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=300,maxheight=300&photoreference=#{restaurant["photos"].first["photo_reference"]}&key=#{ENV['GOOGLE_API_KEY']}"
+      end
+      @restaurant = Restaurant.find_or_create_by(
+        name: restaurant["name"],
+        address: restaurant["formatted_address"],
         zipcode: zipcode,
-        food_type: result['name'],
+        food_type: restaurant["name"],
         hours: hours,
-        website: result['website'],
+        website: restaurant['website'],
         photo: photo,
+        rating: result['rating'],
+        phone: restaurant["formatted_phone_number"]
       )
-      LocationsRestaurant.find_or_create_by(location: @location, restaurant: @restuarant)
+      LocationsRestaurant.find_or_create_by(location: @location, restaurant: @restaurant)
     end
   end
 end
