c92443d1cf26443f6e2203bdc6af30303bad179a
tabs <jtabas@gmail.com>
2017-07-10 11:57:49 -0400

    stylez

--- react/src/components/Restaurant.js
+++ react/src/components/Restaurant.js
@@ -2,10 +2,10 @@ import React from 'react';
 const Restaurant = props => {
   return (
     <div className="restaurant-info-box">
-    <img src={props.photo} alt={props.name} className="restaurant-index-img" />
         <a href={`/restaurants/${props.id}`}>
           <h2>{props.name}</h2>
         </a>
+        <img src={props.photo} alt={props.name} className="restaurant-index-img" />
     </div>
   );
 }
