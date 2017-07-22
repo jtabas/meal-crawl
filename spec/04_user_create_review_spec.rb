f4b893ecc2b5075c823ef153f4705180137c2729
tabs <jtabas@gmail.com>
2017-07-22 17:47:13 -0400

    Fix some tests

---
+++ spec/04_user_create_review_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+xfeature 'Restaurant\'s Reviews Create' do
+  let!(:user) { FactoryGirl.create(:user, email: 'firefly@aol.com') }
+  let!(:restaurant) { FactoryGirl.create(:restaurant, name: "Please") }
+
+  scenario 'User creates a review' do
+    sign_in_as(user)
+    visit restaurant_path(restaurant)
+    fill_in 'Rating', with: 5
+    fill_in 'Your Review', with: 'Awesome Restaurant!!!...JK'
+
+    click_button 'Create Review'
+
+    expect(page).to have_content(5)
+    expect(page).to have_content('Awesome Restaurant!!!...JK')
+  end
+
+  scenario 'User tries to create a review with empty fields' do
+    sign_in_as(user)
+    visit restaurant_path(restaurant)
+    fill_in 'Rating', with: ''
+    fill_in 'Your Review', with: ''
+    click_button 'Create Review'
+
+    expect(page).to have_content('Rating can\'t be blank')
+    expect(page).to have_content('Rating is not a number')
+    expect(page).to have_content('Rating is not a valid numeric rating (Must be between 1-5)')
+  end
+end
