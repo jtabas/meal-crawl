442e1e028dda8502164794d909584641c295bcd4
tabs <jtabas@gmail.com>
2017-07-22 17:27:18 -0400

    Added tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name 'John'
+    last_name 'Smith'
+    sequence(:email) { |n| "john#{n}@gmail.com" }
+    password 'Launch'
+    state 'Pennsylvania'
+    city 'Philadelphia'
+    username 'AtTheRealJohnSmith'
+    admin false
   end
 end
