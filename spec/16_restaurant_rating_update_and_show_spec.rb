b9840a4f0ccd93adfc67e56d26c3ed55795fc942
tabs <jtabas@gmail.com>
2017-08-13 18:48:28 -0400

    restaurant rating test

---
+++ spec/16_restaurant_rating_update_and_show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+xfeature 'user update a restaurant\'s rating' do
+  let!(:restaurant) { FactoryGirl.create(:restaurant) }
+  let!(:user1) { FactoryGirl.create(:user) }
+  let!(:user2) { FactoryGirl.create(:user, email: 'email@email.com') }
+  let!(:review) { FactoryGirl.create(:review, restaurant: restaurant, user: user1) }
+
+  scenario 'User posts a new review which changes the rating' do
+    sign_in_as(user2)
+    visit restaurant_path(restaurant)
+    fill_in 'Rating', with: 4
+    click_button 'Create Review'
+    expect(page).to have_content("Rating: 3.3")
+  end
+end
